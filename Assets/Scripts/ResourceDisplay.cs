dfa9c59c8d2c922cf06a84a59fed26e69199ec7f
Khang <khang.nguyen5700@gmail.com>
2020-07-25 12:11:19 -0600

    Implemented Basic Health and Mana Bars
    Basic Health and Mana bars done.
    
    TODO:
    Need barUI design
    Fiddle with values (do we want it to be a % or actually show the real value on the bar too e.g. 55/100)
    Do we want to include the exp bar on the top panel as well?
    Might need to include a few more methods to be called at other places to update the displays?

---
+++ Assets/Scripts/ResourceDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceDisplay : MonoBehaviour {
+    GameObject playerObject;
+    float maxHealth;
+    float maxMana;
+    Slider healthSlider;
+    Slider manaSlider;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        maxHealth = playerObject.GetComponent<UnitResources>().maxHealth;
+        maxMana = playerObject.GetComponent<UnitResources>().maxMana;
+        healthSlider = gameObject.transform.Find("Health").GetComponent<Slider>();
+        manaSlider = gameObject.transform.Find("Mana").GetComponent<Slider>();        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float currentHealth = playerObject.GetComponent<UnitResources>().GetHealthValue();
+        float currentMana = playerObject.GetComponent<UnitResources>().GetManaValue();
+
+        healthSlider.value = currentHealth / maxHealth;
+        manaSlider.value = currentMana / maxMana;
+    }
+
+    public void updateMaxHealthVal(float newVal) {
+        maxHealth = newVal;
+    }
+    public void updateMaxManaVal(float newVal) {
+        maxMana = newVal;
+    }
+}
